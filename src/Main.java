dbf260121aa6cf1389324334543b8205ff42de8f
liamdono <donovali@mail.uc.edu>
2023-06-11 02:40:24 -0400

    submit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double balance = 5000;
+        double interest = balance * 0.17;
+        System.out.printf("Interest after 1 month is $%.2f\n", interest);
+        balance += interest;
+        System.out.printf("Interest after 2 months is $%.2f\n", interest+balance*0.17);
+    }
+
+}
